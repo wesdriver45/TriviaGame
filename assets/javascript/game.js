49a55c9c279467935f3a640426a3d3e68c84809c
wesdriver45 <wesdriver45@gmail.com>
2017-10-23 21:09:49 -0400

    pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//big start button in center of page
+//timer at the top under title 'It's our time down here:'
+//timer set to 1:30
+//8 ques with 4 multi choice answers each
+//each answer has a circle click
+//compare each answer to right answer
+//game ends at time up or all ques answered
+//play again button at bottom resets game
