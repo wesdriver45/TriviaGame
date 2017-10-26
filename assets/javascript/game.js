ef5322b82a257f35e36c15dc3405b6223d249d11
wesdriver45 <wesdriver45@gmail.com>
2017-10-25 22:07:14 -0400

    js and css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,24 @@
+
+window.onload = function() {
+	$("#gameBox").hide();
+};
+
+
+
+
+$(document).ready(function() {
+//start game function
 //big start button in center of page
+$(".startGame").on("click", function() {
+	$(".startBox").hide();
+	$("#header").hide();
+	$("#gameBox").show();
+	$("#display").show();
+});
 //timer at the top under title 'It's our time down here:'
+	
+
+});
 //timer set to 1:30
 //8 ques with 4 multi choice answers each
 //each answer has a circle click
