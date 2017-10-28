2e0ae36e4c84b60ea41a78a479c664cb267719d0
wesdriver45 <wesdriver45@gmail.com>
2017-10-27 23:18:57 -0400

    js and jq functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,53 @@
 
 window.onload = function() {
 	$("#gameBox").hide();
+	$("#display").hide();
+	$("#scoreBox").hide();
+	
 };
 
-
-
+var right = 0;
+var wrong = 0;
 
 $(document).ready(function() {
 //start game function
 //big start button in center of page
 $(".startGame").on("click", function() {
+//timer set to 60
+	var timer = 60;
+
+	var interval = setInterval(function() {
+    	timer--;
+    	$("#display").text(timer);
+    	if (timer === 0) clearInterval(interval);
+		}, 1000);
+
 	$(".startBox").hide();
 	$("#header").hide();
 	$("#gameBox").show();
 	$("#display").show();
+
+	
+	}
+
 });
-//timer at the top under title 'It's our time down here:'
+
+// $(":radio").on("click, function") {
+// 	if (value === "true") {
+// 		right++;
+// 	} else {
+// 		wrong++;
+// 	}
+// };
+
+
+
 	
 
 });
-//timer set to 1:30
-//8 ques with 4 multi choice answers each
-//each answer has a circle click
+
+//ques function
+
 //compare each answer to right answer
 //game ends at time up or all ques answered
 //play again button at bottom resets game
