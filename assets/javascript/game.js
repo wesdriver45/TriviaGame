e266c17ccdc261eea29d70a95333a07828e86b41
wesdriver45 <wesdriver45@gmail.com>
2017-10-28 08:57:26 -0400

    incomplete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,51 +3,69 @@ window.onload = function() {
 	$("#gameBox").hide();
 	$("#display").hide();
 	$("#scoreBox").hide();
+	$(".resetBox").hide();
 	
 };
 
-var right = 0;
-var wrong = 0;
-
 $(document).ready(function() {
+
+
 //start game function
 //big start button in center of page
 $(".startGame").on("click", function() {
-//timer set to 60
-	var timer = 60;
 
-	var interval = setInterval(function() {
-    	timer--;
-    	$("#display").text(timer);
-    	if (timer === 0) clearInterval(interval);
-		}, 1000);
 
 	$(".startBox").hide();
 	$("#header").hide();
 	$("#gameBox").show();
 	$("#display").show();
 
+
+//timer set to 60
+	var time = 5;
+	var timer = setInterval(countDown, 1000);
+
+	function countDown() {
+		time--;
+		if (time === 0) {
+			stop();
+}
+	$("#display").text(time);
+}
+
+	function stop() {
+		clearInterval(timer);
+		$("#scoreBox").show();
+		$("#header").show();
+		$(".resetBox").show();
+		$("#gameBox").hide();
+	}		
 	
-	}
-
-});
-
-// $(":radio").on("click, function") {
-// 	if (value === "true") {
-// 		right++;
-// 	} else {
-// 		wrong++;
-// 	}
-// };
-
-
-
+	
 	
 
 });
 
 //ques function
 
-//compare each answer to right answer
+
+// var right = 0;
+// var wrong = 0;
+
+// $(":radio").on("click, function") {
+// 	if (value === "true") {
+// 		right++;
+//		$("#right").text(right);
+// 	} else {
+// 		wrong++;
+//		$("#wrong").text(wrong);
+// 	}
+// };
+
+
 //game ends at time up or all ques answered
 //play again button at bottom resets game
+
+});
+
+
